7880fe7ee9f74fe2e98083b2bec0a9188854047e
SantiCaste <45802472+SantiCaste@users.noreply.github.com>
2026-04-02 21:36:14 -0300

    Add: single-neuron - submission-1

---
+++ Machine Learning/single-neuron/submission-1.py
@@ -0,0 +1,27 @@
+import numpy as np
+from numpy.typing import NDArray
+
+
+class Solution:
+    def forward(self, x: NDArray[np.float64], w: NDArray[np.float64], b: float, activation: str) -> float:
+        # x: 1D input array
+        # w: 1D weight array (same length as x)
+        # b: scalar bias
+        # activation: "sigmoid" or "relu"
+        #
+        # Pre-activation: z = dot(x, w) + b
+        # Sigmoid: σ(z) = 1 / (1 + exp(-z))
+        # ReLU: max(0, z)
+        # return round(your_answer, 5)
+        # pass
+
+        # option 1:
+        #z = np.dot(x, w) + b
+        z = np.sum(x * w) + b
+        a = 0.
+        if activation == "sigmoid":
+            a = 1 / (1 + np.exp(-z))
+        elif activation == "relu":
+            a = max(0., z)
+
+        return round(a, 5)
